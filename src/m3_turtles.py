b8b12bb52dc74ad4599c75d721be72588495daaa
whitacbc <whitacbc@rose-hulman>
2017-11-27 15:00:47 -0500

    finished M3

--- src/m3_turtles.py
+++ src/m3_turtles.py
@@ -14,7 +14,7 @@ Authors: David Mutchler, Dave Fisher, Valerie Galluzzi, Amanda Stouder,
 """
 ########################################################################
 #
-# TODO: 1.
+# DONE: 1.
 #  (Yes, that means for YOU to DO things per these instructions:)
 #
 # On Line 13 above, replace  PUT_YOUR_OWN_NAME_HERE  with your OWN name.
@@ -29,7 +29,7 @@ import rosegraphics as rg
 
 ########################################################################
 #
-# TODO: 2.
+# DONE: 2.
 #   Allow this file to use the rosegraphics.py file by marking the src
 #   directory as a "Sources Root".  Do that by right clicking on the src folder,
 #   then selector  Mark Directory As --> Sources Root
@@ -61,8 +61,11 @@ dave = rg.SimpleTurtle()
 # Ask the SimpleTurtle objects to do things:
 # ----------------------------------------------------------------------
 dave.forward(100)
-dave.left(90)
+dave.left(45)
 dave.forward(200)
+dave.right(45)
+dave.backward(400)
+
 
 # ----------------------------------------------------------------------
 # Construct a new turtle and ask it to do things.
@@ -73,11 +76,13 @@ matt.speed = 10  # Faster
 matt.backward(50)
 matt.left(90)
 matt.forward(50)
+matt.right (45)
+matt.backward(400)
 
 
 ########################################################################
 #
-# TODO: 3.
+# DONE: 3.
 #   Add a few more line of your own code above to make one of the
 #   existing Turtles move some more and/or have different
 #   characteristics.
@@ -91,7 +96,7 @@ matt.forward(50)
 
 ########################################################################
 #
-# TODO: 4.
+# DONE: 4.
 #   The code above  CONSTRUCTS  two SimpleTurtle objects and gives those objects NAMES:
 #       dave    matt
 #
@@ -109,10 +114,13 @@ matt.forward(50)
 #   As always, test by running the module.
 #
 ########################################################################
+John = rg.SimpleTurtle("turtle")
+John.forward(100)
+John.draw_circle(100)    #makes john draw a circle
 
 ########################################################################
 #
-# TODO: 5.
+# DONE: 5.
 #   Run one more time to be sure that all is still OK.
 #   Ensure that no blue bars on the scrollbar-thing to the right remain.
 #
