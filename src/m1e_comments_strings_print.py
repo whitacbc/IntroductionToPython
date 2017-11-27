82a9d6eaa858dd60e0bd86dd71a983c0f5a16d69
whitacbc <whitacbc@rose-hulman>
2017-11-27 14:34:53 -0500

    I added my name

--- src/m1e_comments_strings_print.py
+++ src/m1e_comments_strings_print.py
@@ -2,7 +2,7 @@
 #
 #   Run this module by right clicking anywhere in this window, then select
 #        Run 'name of file'
-#   After running, find the Console tab (below) and confirm that everything
+#   After running, find the Consohttps://github.com/whitacbc/IntroductionToPython.gitle tab (below) and confirm that everything
 #   did indeed get printed (displayed).  This module is just an example.
 #   Once you have run the file, feel free to play with it some, then you
 #   can move on to m2.
